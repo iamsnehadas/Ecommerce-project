a719aa91bcc45835da48f7c709af2b2c9c1759ea
iamsnehadas <snehasnow8@gmail.com>
2025-08-22 03:19:45 +0530

    id changed

--- scripts/products.js
+++ scripts/products.js
@@ -1,72 +1,72 @@
 export const products = [
     {
-        id: '1ghgw615718291',
+        id: '5s6g7m4h0v4h6m8l',
         name: 'Vintage Leather Diary - 2025',
         pricePaise: 39900,
         imageAlt: 'leather-diary'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '9i3psg9p3m0gqf3m',
         name: 'Aesthetic Pen and Pencil Holder',
         pricePaise: 25000,
         imageAlt: 'aesthetic-penstand'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '5v4t7v2jud5t0u3r',
         name: 'Artisanal Coffee Beans 50g',
         pricePaise: 29900,
         imageAlt: 'Artisanal Coffee Beans'
     },
     {
-        id: '1ghgw615718291',
+        id: '4h7o0s8k9r7o6p5t',
         name: 'Fountain Pen with Blue Ink',
         pricePaise: 49900,
         imageAlt: 'Fountain pen'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '5g1f2u2w3w0q7g3q',
         name: 'Handmade Vintage Utensils set',
         pricePaise: 75000,
         imageAlt: 'Handmade utensil set'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '6o8e5g0t6q1l7w6s',
         name: 'Set of 2 Handmade Ceramic Cups',
         pricePaise: 29900,
         imageAlt: 'handmade-ceramic-cup'
     },
     {
-        id: '1ghgw615718291',
+        id: '1i2w5e2v8r0q3q6d',
         name: 'Handmade Aesthetic Plates (Set of 1)',
         pricePaise: 34900,
         imageAlt: 'Handmade-plate'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '6v2f0l7f6t1q8w2m',
         name: 'Handwoven Warm Brown Scarf',
         pricePaise: 59900,
         imageAlt: 'Handsewn-scarf'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '0s5o0d2o9v7h1r2l',
         name: 'Lavendar Scented Candle',
         pricePaise: 29900,
         imageAlt: 'Lavendar-scented-candle'
     },
     {
-        id: '1ghgw615718291',
+        id: '8v3w2k5p6g4l3j6i',
         name: 'Minimalist Grey Penstand',
         pricePaise: 38950,
         imageAlt: 'Penstand-minimalist'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '4q0l4l2f3r7k1s8n',
         name: 'Minimalist Daily Planner - No Date',
         pricePaise: 19900,
         imageAlt: 'Planner'
     },
     {
-        id: 'wsfghjuy123tgh',
+        id: '9p4f6d1k6s2m0m2k',
         name: 'Customisable Resin Pendant',
         pricePaise: 38900,
         imageAlt: 'Resin Pendant'
